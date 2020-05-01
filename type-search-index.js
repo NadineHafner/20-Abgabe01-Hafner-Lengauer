b43fccd4354dbeb5f5bbad057713334cefcd7157
Johannes Lengauer <Johannes_lengauer@hotmail.com>
2020-05-01 17:27:30 +0200

    Removed JavaDocsData from mainfolder

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"at.fhj.iit","l":"ComplexDrink"},{"p":"at.fhj.iit","l":"Drink"},{"p":"at.fhj.iit","l":"FashionedDrink"},{"p":"at.fhj.iit","l":"Liquid"},{"p":"at.fhj.iit","l":"LiquidTest"},{"p":"at.fhj.iit","l":"SimpleDrink"},{"p":"at.fhj.iit","l":"TemperedDrink"}]
